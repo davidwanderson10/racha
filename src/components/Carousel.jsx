31967064b182e6922d63acbdd2f1956b986e9d61
David Wanderson <59055735+davidwanderson10@users.noreply.github.com>
2025-01-22 17:19:56 -0300

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,6 +9,11 @@ import patrocicio4 from "../assets/apoio4.png";
 import patrocicio5 from "../assets/apoio5.png";
 import patrocicio6 from "../assets/apoio6.png";
 import patrocicio7 from "../assets/apoio7.png";
+import patrocicio8 from "../assets/apoio8.png";
+import patrocicio9 from "../assets/apoio9.png";
+import patrocicio10 from "../assets/apoio10.png";
+import patrocicio11 from "../assets/apoio11.png";
+import patrocicio12 from "../assets/apoio12.png";
 
 
 const Carousels = () => {
@@ -23,14 +28,19 @@ const Carousels = () => {
     prevArrow: <SamplePrevArrow />, // Personalizar seta para anterior
   };
 
-  const logosInput = [
+const logosInput = [
     { id: 1, src: patrocicio1, alt: "Logo 1", link: "https://www.instagram.com/yessodonto" },
     { id: 2, src: patrocicio2, alt: "Logo 2", link: "https://www.instagram.com/regilabenigno" },
     { id: 3, src: patrocicio3, alt: "Logo 3", link: "https://www.instagram.com/nogueirasexecutive" },
     { id: 4, src: patrocicio4, alt: "Logo 4", link: "https://www.instagram.com/gustavosimaofotografo" },
     { id: 5, src: patrocicio5, alt: "Logo 5", link: "https://www.instagram.com/yessacai" },
     { id: 6, src: patrocicio6, alt: "Logo 6", link: "https://www.instagram.com/greenvidros" },
-    { id: 7, src: patrocicio7, alt: "Logo 7", link: "https://www.instagram.com/clinicapsicmed" }
+    { id: 7, src: patrocicio7, alt: "Logo 7", link: "https://www.instagram.com/clinicapsicmed"},
+    { id: 8, src: patrocicio8, alt: "Logo 8", link: "https://www.instagram.com/_jmcarestetica"},
+    { id: 9, src: patrocicio9, alt: "Logo 9", link: "https://www.instagram.com/garagem_maraponga"},
+    { id: 10, src: patrocicio10, alt: "Logo 10", link: "https://www.instagram.com/svoiceoficial"},
+    { id: 11, src: patrocicio11, alt: "Logo 11", link: "https://www.instagram.com/schatoficial"},
+    { id: 12, src: patrocicio12, alt: "Logo 12", link: "https://www.instagram.com/use.smartwatch"}
   ];
 
 
